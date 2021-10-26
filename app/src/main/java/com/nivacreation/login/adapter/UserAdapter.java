ad104f8111d8d9d7bbbbd2bbab2dd0a423762889
Nivesh Wanninayaka <niveshncwanninayaka@gmail.com>
2021-10-27 00:55:57 +0530

    Successfully user Updating and Deleting by Admin

--- app/src/main/java/com/nivacreation/login/adapter/UserAdapter.java
+++ app/src/main/java/com/nivacreation/login/adapter/UserAdapter.java
@@ -1,25 +1,45 @@
 package com.nivacreation.login.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.nivacreation.login.R;
 import com.nivacreation.login.model.User;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
 
     Context context;
     ArrayList<User> userArrayList;
+
+    FirebaseFirestore fStore;
 
     public UserAdapter(Context context, ArrayList<User> userArrayList) {
         this.context = context;
@@ -44,8 +64,129 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         holder.lastName.setText(user.getLastName());
         holder.userType.setText(user.getUserType());
         holder.email.setText(user.getEmail());
+        holder.contact.setText(user.getContact());
+
+        holder.contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+user.getContact()));
+                context.startActivity(intent);
+            }
+        });
+
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.email.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.update_popup))
+                        .setExpanded(true,1050)
+                        .create();
+
+                View view = dialogPlus.getHolderView();
+
+                EditText firstName =view.findViewById(R.id.txtFirstName);
+                EditText lastName =view.findViewById(R.id.txtLastName);
+                EditText contact =view.findViewById(R.id.txtContact);
+                EditText city =view.findViewById(R.id.txtCity);
+                EditText street =view.findViewById(R.id.txtStreet);
+
+                Button btnUpdate =view.findViewById(R.id.btnUpdate);
+
+                firstName.setText(user.getFirstName());
+                lastName.setText(user.getLastName());
+                contact.setText(user.getContact());
+                city.setText(user.getCity());
+                street.setText(user.getStreet());
+
+                contact.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                        if (validateMobile(contact.getText().toString())){
+
+                        }else{
+                            contact.setError("Invalid Mobile No! First Digit 0, Include 10 Digits!");
+
+                        }
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
 
 
+                dialogPlus.show();
+
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Map<String,Object> map = new HashMap<>();
+
+                        map.put("firstName",firstName.getText().toString());
+                        map.put("lastName",lastName.getText().toString());
+                        map.put("contact",contact.getText().toString());
+                        map.put("city",city.getText().toString());
+                        map.put("street",street.getText().toString());
+
+                        FirebaseFirestore.getInstance().collection("Users").document(user.getUserID()).update(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.firstname.getContext(),"Data Updated Successfully!",Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull @NotNull Exception e) {
+                                        Toast.makeText(holder.firstname.getContext(),"Error While Updating!",Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+
+                    }
+                });
+            }
+        });
+
+
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(holder.firstname.getContext());
+                builder.setTitle("Deleting User");
+                builder.setMessage("Are you Sure Want to Delete the user?");
+
+                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        FirebaseFirestore.getInstance().collection("Users").document(user.getUserID()).delete();
+                        Toast.makeText(holder.firstname.getContext(),"User successfully deleted.!",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.show();
+            }
+        });
 
     }
 
@@ -57,7 +198,9 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
     public static class UserViewHolder extends RecyclerView.ViewHolder{
 
-        TextView firstname, lastName, userType, email;
+        TextView firstname, lastName, userType, email,contact;
+
+        Button btnEdit, btnDelete;
 
         public UserViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
@@ -66,6 +209,17 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
             lastName = itemView.findViewById(R.id.userLastName);
             userType = itemView.findViewById(R.id.userType);
             email = itemView.findViewById(R.id.userEmail);
+            contact =itemView.findViewById(R.id.contact);
+
+            btnEdit = itemView.findViewById(R.id.updateBtn);
+            btnDelete = itemView.findViewById(R.id.deleteBtn);
         }
+    }
+    boolean validateMobile(String input){
+
+        Pattern p = Pattern.compile("[0][0-9]{9}");
+        Matcher m = p.matcher(input);
+
+        return m.matches();
     }
 }
--- app/src/main/java/com/nivacreation/login/model/User.java
+++ app/src/main/java/com/nivacreation/login/model/User.java
@@ -2,21 +2,35 @@ package com.nivacreation.login.model;
 
 public class User {
 
-    String firstName, lastName, userType, email;
+    String firstName, lastName, userType, email,contact, city, street,userID;
+
+
 
     public User() {
 
     }
 
-    public User(String firstName, String lastName, String userType, String email) {
+    public User(String firstName, String lastName, String userType, String email, String contact, String city, String street, String userID) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.userType = userType;
         this.email = email;
+        this.city = city;
+        this.contact =contact;
+        this.street = street;
+        this.userID = userID;
     }
 
     public String getFirstName() {
         return firstName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
 
     public void setFirstName(String firstName) {
@@ -45,5 +59,29 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
     }
 }
